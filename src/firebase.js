f047c997b00b941f0bb045b4006e808268cf7e8d
Lucas <r.lucasmoretto@gmail.com>
2022-10-01 00:10:02 -0300

    add env

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwW6TooP3IHRwA4JkRO1MzdKG2uc-dWPc",
-  authDomain: "ecommerce-87e77.firebaseapp.com",
-  databaseURL: "https://ecommerce-87e77-default-rtdb.firebaseio.com",
-  projectId: "ecommerce-87e77",
-  storageBucket: "ecommerce-87e77.appspot.com",
-  messagingSenderId: "329008221347",
-  appId: "1:329008221347:web:ede74c3b8c7bb3adc00316",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
