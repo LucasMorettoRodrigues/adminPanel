93cd8f0b967be5b6527e7840e6a5b0f4c776ee7c
Lucas <r.lucasmoretto@gmail.com>
2022-10-19 10:20:27 -0300

    edit account

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,40 +1,27 @@
-import Head from 'next/head';
-import { Box, Container, Grid, Typography } from '@mui/material';
-import { AccountProfileDetails } from '../components/account/account-profile-details';
-import { DashboardLayout } from '../components/dashboard-layout';
+import Head from "next/head";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { AccountProfileDetails } from "../components/account/account-profile-details";
+import { DashboardLayout } from "../components/dashboard-layout";
 
 const Page = () => (
   <>
     <Head>
-      <title>
-        Minha Conta
-      </title>
+      <title>Minha Conta</title>
     </Head>
     <Box
       component="main"
       sx={{
         flexGrow: 1,
-        py: 8
+        py: 8,
       }}
     >
       <Container maxWidth="lg">
-        <Typography
-          sx={{ mb: 3 }}
-          variant="h4"
-        >
+        <Typography sx={{ mb: 3 }} variant="h4">
           Minha Conta
         </Typography>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={12}
-            md={12}
-            xs={12}
-          >
-            <AccountProfileDetails />
+        <Grid container spacing={3}>
+          <Grid item lg={12} md={12} xs={12}>
+            {/* <AccountProfileDetails /> */}
           </Grid>
         </Grid>
       </Container>
@@ -42,10 +29,6 @@ const Page = () => (
   </>
 );
 
-Page.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default Page;
