8b73eee420139842ec078f90991a523e9aa4e0e3
Lucas <r.lucasmoretto@gmail.com>
2022-10-19 11:03:28 -0300

    edit notifications

--- src/pages/api/notifications.js
+++ src/pages/api/notifications.js
@@ -4,17 +4,16 @@ import { v4 as uuid } from "uuid";
 export default async function handler(req, res) {
   const body = req.body;
 
-  console.log(body);
+  const notificationsService = new NotificationsService();
+
+  notificationsService.put(uuid(), body);
+
   res.status(200).json({ body });
 
   // if (body.charges[body.charges.length - 1].status !== "PAID") {
   //   res.status(200).json({ status: "success" });
   //   return;
   // }
-
-  // const notificationsService = new NotificationsService();
-
-  // notificationsService.put(uuid(), body);
 
   // res.status(200).json({ status: "success" });
 }
