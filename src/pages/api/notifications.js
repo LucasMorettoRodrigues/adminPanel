f12dd9f3ca5e047dd4827def18da0c235d4d5acd
Lucas <r.lucasmoretto@gmail.com>
2022-10-19 11:00:16 -0300

    edit notifications

--- src/pages/api/notifications.js
+++ src/pages/api/notifications.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const body = req.body;
 
   console.log(body);
-  res.status(200).json({ body, teste: body.charges });
+  res.status(200).json({ body });
 
   // if (body.charges[body.charges.length - 1].status !== "PAID") {
   //   res.status(200).json({ status: "success" });
